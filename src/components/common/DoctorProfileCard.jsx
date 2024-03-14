c1db7f52620d440524e0ce484b5fe5a384d99e20
sanidhyasin <singhsanidhya741@gmail.com>
2024-03-14 06:12:37 +0530

    feat: many new pages

--- src/components/common/DoctorProfileCard.jsx
+++ src/components/common/DoctorProfileCard.jsx
@@ -1,4 +1,6 @@
-const DoctorProfileCard = () => {
+import { useState } from "react";
+
+const DoctorProfileCard = (props) => {
   return (
     <div>
       <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
@@ -81,7 +83,9 @@ const DoctorProfileCard = () => {
             <h7>Cardiologist</h7>
           </div>
           <a
-            href="#"
+            // href="dashboard/76"
+            // eslint-disable-next-line react/prop-types
+            onClick={() => props.setDoctorsCards(false)}
             className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
             <svg
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/common/Navbar";
+import Home from "../components/home/Home";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+};
+
+export default Homepage;
